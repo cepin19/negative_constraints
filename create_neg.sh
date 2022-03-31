c6e91d9ac9d8adde4cc85bd252ef4496fc8992be
Josef Jon <pepa@lingea.cz>
2022-03-31 17:20:01 +0200

    start

--- create_neg.sh
+++ create_neg.sh
@@ -2,7 +2,7 @@ python3 generate_neg_constraints.py
 for f in outputs/*negative_constraints
 do 
 	echo $f
-	~/marian-constraints/spm_encode --model ../neg/corp/encs.spm < $f > $f.sp_newsp
+	~/marian-constraints/build/spm_encode --model ../neg/corp/encs.spm < $f > $f.sp_newsp
 	cat $f.sp_newsp | python3  convert_constraints_to_yaml.py > $f.sp_newsp.yml
 	cat $f.sp_newsp | python3 convert_constraints_to_yaml_no_multi.py > $f.sp_newsp.nomulti.yml
     cat $f.sp_newsp | python3  convert_constraints_to_suffix.py > $f.sp_newsp.suffix
